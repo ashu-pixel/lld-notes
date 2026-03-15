ae8e5873bb5fb75a7dac8ea4e854e71d60441dee
ashu-pixel <ashutoshgupta1704@gmail.com>
2026-03-15 17:09:37 +0530

    more examples

---
+++ src/main/java/com/MultithreadingQuestions/Medium/PingPongUsingReentrantLock.java
@@ -0,0 +1,83 @@
+package com.MultithreadingQuestions.Medium;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+Note - 
+one of the characteristics of ReentrantLock, that differentiates it from Semaphore, 
+is that after a lock is aquired on ReentrantLock, it can only be released by the thread that holds the lock. 
+In case of Semaphore, a permit can be released from any thread. */
+
+class PingPong {
+    private int n;
+    private ReentrantLock lock = new ReentrantLock();
+    private final Condition pingCondition = lock.newCondition();
+    private final Condition pongCondition = lock.newCondition();
+    private boolean isPingTurn = true;
+
+    public PingPong(int n) {
+        this.n = n;
+    }
+
+    public void ping() throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            lock.lock();
+            try {
+                while (!isPingTurn) {
+                    pingCondition.await();
+                }
+                System.out.println("ping ");
+                isPingTurn = false;
+                pongCondition.signal();
+            } finally {
+                /* This is always required if not done program will be stuck forever */
+                lock.unlock();
+            }
+        }
+    }
+
+    public void pong() throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            lock.lock();
+            try {
+                while (isPingTurn) {
+                    pongCondition.await();
+                }
+                System.out.println("pong ");
+                isPingTurn = true;
+                pingCondition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
+
+public class PingPongUsingReentrantLock {
+
+    public static void main(String[] args) {
+
+        PingPong pingPong = new PingPong(5);
+
+        Thread pingThread = new Thread(() -> {
+            try {
+                pingPong.ping();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread pongThread = new Thread(() -> {
+            try {
+                pingPong.pong();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        pingThread.start();
+        pongThread.start();
+    }
+
+}
---
+++ src/main/java/com/MultithreadingQuestions/Medium/ZeroOddEven.java
@@ -0,0 +1,100 @@
+package com.MultithreadingQuestions.Medium;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+Q. Write a program that outputs the series:
+ 
+0 1 0 2 0 3 0 4 0 5...  
+zero is called first, then odd and even numbers are called in increasing order.
+*/
+
+class ZeroEvenOddUsingReentrantLock {
+    private int n;
+    private int state = 0;
+
+    private ReentrantLock lock = new ReentrantLock();
+    private final Condition condition0 = lock.newCondition();
+    private final Condition conditionOdd = lock.newCondition();
+    private final Condition conditionEven = lock.newCondition();
+
+    public ZeroEvenOddUsingReentrantLock(int n) {
+        this.n = n;
+    }
+
+    public void zero() {
+        for (int i = 1; i <= n; i++) {
+            lock.lock();
+            try {
+                while (state != 0) {
+                    condition0.await();
+                }
+                System.out.print(0 + " ");
+                if (i % 2 == 1) {
+                    state = 1;
+                    conditionOdd.signal();
+                } else {
+                    state = 2;
+                    conditionEven.signal();
+                }
+            } catch (Exception e) {
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void even() {
+        for (int i = 2; i <= n; i += 2) {
+
+            lock.lock();
+            try {
+                while (state != 2) {
+                    conditionEven.await();
+                }
+                System.out.print(i + " ");
+                state = 0;
+                condition0.signal();
+            } catch (Exception e) {
+            } finally {
+                lock.unlock();
+            }
+
+        }
+    }
+
+    public void odd() {
+        for (int i = 1; i <= n; i += 2) {
+
+            lock.lock();
+            try {
+                while (state != 1) {
+                    conditionOdd.await();
+                }
+                System.out.print(i + " ");
+                state = 0;
+                condition0.signal();
+            } catch (Exception e) {
+            } finally {
+                lock.unlock();
+            }
+
+        }
+    }
+}
+
+public class ZeroOddEven {
+
+    public static void main(String[] args) throws InterruptedException {
+        ZeroEvenOddUsingReentrantLock zeroEvenOdd = new ZeroEvenOddUsingReentrantLock(5);
+
+        Thread zeroThread = new Thread(zeroEvenOdd::zero);
+        Thread evenThread = new Thread(zeroEvenOdd::even);
+        Thread oddThread = new Thread(zeroEvenOdd::odd);
+
+        zeroThread.start();
+        evenThread.start();
+        oddThread.start();
+    }
+}
