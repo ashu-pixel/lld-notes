be8fd4a7f484f3581814898173b41ab300bda780
ashu-pixel <ashutoshgupta1704@gmail.com>
2026-03-16 11:44:49 +0530

    callable and future

--- src/main/java/com/MultithreadingQuestions/Medium/ExecutorService/B_CallableNFuture.java
+++ src/main/java/com/MultithreadingQuestions/Medium/ExecutorService/B_CallableNFuture.java
@@ -1,5 +1,9 @@
 package com.MultithreadingQuestions.Medium.ExecutorService;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class B_CallableNFuture {
     public static void main(String[] args) {
 
@@ -21,8 +25,27 @@ public class B_CallableNFuture {
             System.out.println("Exception caught");
         }
 
-        // To overcome limitation of not returning result we can use our custom class BA_CustomCallable 
-        // Clean way to overcome these limitations is to use Callable and Future 
+        // To overcome limitation of not returning result we can use our custom class
+        // BA_CustomCallable
+        // Clean way to overcome these limitations is to use Callable and Future
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        // Note here we are passing callable and not runnable, so we can return result
+        // and throw checked exception
+        Future<Integer> future = executorService.submit(() -> {
+            throw new RuntimeException("Checked exception thrown from thread");
+        });
+
+        try {
+            future.get();
+        } catch (Exception e) {
+            // Able to get exception thrown from thread and handle it in main thread
+            System.out.println("Exception caught from future");
+        }
+        executorService.shutdown();
+
+        // https://www.geeksforgeeks.org/java/completablefuture-in-java/
 
     }
 
