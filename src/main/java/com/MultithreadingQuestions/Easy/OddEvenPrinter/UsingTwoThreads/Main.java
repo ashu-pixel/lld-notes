c86fb70b3c8f0ef8a8a9ac9f54ceaa647e7f5307
ashu-pixel <ashutoshgupta1704@gmail.com>
2026-04-10 10:34:06 +0530

    even odd using Semaphore

--- src/main/java/com/MultithreadingQuestions/Easy/OddEvenPrinter/UsingTwoThreads/Main.java
+++ src/main/java/com/MultithreadingQuestions/Easy/OddEvenPrinter/UsingTwoThreads/Main.java
@@ -1,18 +1,60 @@
 package com.MultithreadingQuestions.Easy.OddEvenPrinter.UsingTwoThreads;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
         SharedClass sharedClass = new SharedClass(25);
 
-        /* Thread thread1 = new Thread(new EvenPrinter(sharedClass), "Even Printer");
-        Thread thread2 = new Thread(new OddPrinter(sharedClass), "Odd Printer"); */
-        // If you see we are repeating the logic in both the EvenPrinter and OddPrinter class, 
-        // we can create a common Printer class and pass the threadId to it, 
+        /* Approach 1 */
+        /*
+         * Thread thread1 = new Thread(new EvenPrinter(sharedClass), "Even Printer");
+         * Thread thread2 = new Thread(new OddPrinter(sharedClass), "Odd Printer");
+         */
+        // If you see we are repeating the logic in both the EvenPrinter and OddPrinter
+        // class,
+        // we can create a common Printer class and pass the threadId to it,
         // which will decide whether to print odd or even number.
 
-        Thread thread1 = new Thread(new Printer(sharedClass, 0), "Even Printer");
-        Thread thread2 = new Thread(new Printer(sharedClass, 1), "Odd Printer");
-        thread1.start();
-        thread2.start();
+        /* Approach 2 */
+        /*
+         * Thread thread1 = new Thread(new Printer(sharedClass, 0), "Even Printer");
+         * Thread thread2 = new Thread(new Printer(sharedClass, 1), "Odd Printer");
+         * thread1.start();
+         * thread2.start();
+         */
+
+        /* Approach 3 */
+        Semaphore odd = new Semaphore(1);
+        Semaphore even = new Semaphore(0);
+        Thread t1 = new Thread(() -> {
+            for (; sharedClass.getCurrentNumber() <= sharedClass.getMAX_LIMIT();) {
+                try {
+                    odd.acquire();
+                    Thread.sleep(1000);
+                    if (sharedClass.getCurrentNumber() <= sharedClass.getMAX_LIMIT())
+                        sharedClass.print();
+
+                    even.release();
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (; sharedClass.getCurrentNumber() <= sharedClass.getMAX_LIMIT();) {
+                try {
+                    even.acquire();
+                    Thread.sleep(1000);
+                    if (sharedClass.getCurrentNumber() <= sharedClass.getMAX_LIMIT())
+                        sharedClass.print();
+                    odd.release();
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
     }
 }
